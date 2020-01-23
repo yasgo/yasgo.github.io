85f6463a7288d66ca1c83f85eceb6f3a996fb452
yasinkalkan <yasin.kalkan@medyasoft.com.tr>
2020-01-23 10:36:24 +0300

    chalalnge edit commit

--- challange/4/service-worker.js
+++ challange/4/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/challange/4/precache-manifest.3d35bda814267bee818e48cb44351fa1.js"
+  "/precache-manifest.911f86f38dc520e64b1d7b0cedf25c1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challange/4/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
