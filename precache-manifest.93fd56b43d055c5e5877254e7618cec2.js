680acc578066afe979259539af829ddcce4fe725
yasgo <yasinburakkalkan@gmail.com>
2021-01-13 20:26:04 +0300

    Updates

---
+++ precache-manifest.93fd56b43d055c5e5877254e7618cec2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d06d4d32e4c9b4ab5c16857086c325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c8fc52bdaeef6d3d19b",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "e5b8c01e443bab46efbb",
+    "url": "/static/css/main.9c4edce9.chunk.css"
+  },
+  {
+    "revision": "6c8fc52bdaeef6d3d19b",
+    "url": "/static/js/2.07f56a41.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.07f56a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b8c01e443bab46efbb",
+    "url": "/static/js/main.c25e0a7e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "0c50e82b60d542fafb5a299c10bc843f",
+    "url": "/static/media/animation.0c50e82b.gif"
+  },
+  {
+    "revision": "34be046f70411fd3d65f9b2ca9979d26",
+    "url": "/static/media/icomoon.34be046f.eot"
+  },
+  {
+    "revision": "488c69066d935405fb5301a3a4c057bd",
+    "url": "/static/media/icomoon.488c6906.woff"
+  },
+  {
+    "revision": "515b1163c65ab418ef6da125cec38812",
+    "url": "/static/media/icomoon.515b1163.svg"
+  },
+  {
+    "revision": "9f3e4b2dd1a85a209c407e009a3a320c",
+    "url": "/static/media/icomoon.9f3e4b2d.ttf"
+  },
+  {
+    "revision": "52863eb01a91128608baff016bff8c17",
+    "url": "/static/media/me.52863eb0.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
